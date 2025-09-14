da2588d7d749c48139adc1933536b65f17122ca0
Randeepa Ariyawansa <148050843+Randeepa23@users.noreply.github.com>
2025-09-14 20:21:51 +0530

    Remove logo text from header navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
                 isScrolled ? "text-primary" : "text-white"
               }`}
             >
-              BizConnect
+              
             </a>
           </div>
 
